010e54cbd3fe4ccdf065726ae7e15a45b02f4b22
Avinash Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2022-10-10 21:20:56 -0400

    Changed survey file.

--- zetasurvey.m
+++ zetasurvey.m
@@ -3,50 +3,44 @@ host := "localhost";
 port := 10000;
 Attach("CubicLib.m");
 
-AttachSpec("~/magma-parallel-cookbook/spec");
-socket := Socket(: LocalHost := "localhost", LocalPort := 10000);
+// Set test flag.
+PARALLEL_MODE := false;
 
+if PARALLEL_MODE then
+    AttachSpec("~/magma-parallel-cookbook/spec");
+    socket := Socket(: LocalHost := "localhost", LocalPort := 10000);
 
+    // Load database
+    orbdata := LoadCubicOrbitData(: Flat:=true); // 2 minute load.
 
-// Load database.
-//load "read-data-eof.m"; 
-orbdata := LoadCubicOrbitData(: Flat:=true); // 2 minute load.
+    // Launch!
+    StartDistributedWorkers("zeta-worker.m", 40);
+    results := DistributedManager(socket, orblist);
 
-// Load the bit conversion function;
+    print "";
+    
+    // Resource cleanup.
+    delete socket;
+    
+else
+    // Load Point counting function.
+    // load "computecharpoly.m";
+    orbdata := LoadCubicOrbitData(: Flat:=false); // 2 minute load.
 
-// Loads orbdata, an array of 85 arrays.
-// Each entry of the inner array is a length 56 bit string, 
+    for i in [1..85] do
+        for j in [1..1] do
+            b := orbdata[i][j]; // For example;
 
-//orblist := [f : f in &cat orbdata | Seqint(f,2) ne 0];
+            try
+	        f := BitListToCubic(b);
+	        a := PointCounts(f);
+	        print a; // Want to parallelize this call.
+	        WriteZetaData(i, j, IsSmooth(f), a);
+            catch e
+	        print e;
+            end try;
+        end for;
+    end for;
+    
+end if;
 
-//StartDistributedWorkers("zeta-worker.m", 40);
-//results := DistributedManager(socket, orblist);
-
-
-// Print something to get the prompt back.
-print "";
-
-// b @@ bit to converts to cubic.
-
-// Check if it is smooth.
-
-// Loop PointsCounts over all smooth orbit classes.
-
-
-for i in [1..85] do
-    b := orbdata[i]; // For example;
-
-    try
-	f := BitListToCubic(b);
-	a := PointCounts(f);
-	print a; // Want to parallelize this call.
-	WriteZetaData(i, 1, IsSmooth(f), a);
-    catch e
-	print e;
-    end try;
-end for;
-
-
-
-// Resource cleanup.
-delete socket;
