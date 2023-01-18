fe661fd364349784da41883475336afddcbf3f9e
Jonah R. Weinbaum <f004twj@discovery7.hpcc.dartmouth.edu>
2023-01-18 12:56:15 -0500

    Added job array submission

--- CubicLib/point_counting_cpp/submit_discovery.sh
+++ CubicLib/point_counting_cpp/submit_discovery.sh
@@ -1,8 +1,21 @@
-# loop over all fastq files in the directory, print the filename and submit the gzip jobs to SLURM
-#
- 
-for FILE build/count_bigq_*; do
-    echo ${FILE}
-    sbatch -n 1 -t 1-00:00 --wrap="./${FILE}"
-    sleep 1 # pause to be kind to the scheduler
-done
+#!/bin/bash -l
+
+# Name of the job
+#SBATCH --job-name=count_bigq
+
+# Number of cores
+#SBATCH --ntasks-per-node=1
+
+# Array jobs.  This example will create 25 jobs
+#SBATCH --array=1-25
+
+# Walltime (job duration)
+#SBATCH --time=00:15:00
+
+# Email notifications
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+./count_bigq_$SLURM_ARRAY_TASK_ID
+
+sleep 300
+hostname -s
