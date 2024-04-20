d29d2c000f539ebdcd9baebbb25fdba8a8f49f65
Avinash Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2024-04-20 01:32:40 -0700

    Update planes-compute.m

--- src/linear_space_calculations/planes-compute.m
+++ src/linear_space_calculations/planes-compute.m
@@ -1,53 +1,84 @@
+AttachSpec("../CubicLib/CubicLib.spec");
 
-Attach("CubicLib.spec");
-
-/*planes is the 1395 echelon forms cutting out planes in P5. fourlines is a choice of four lines on each cubic, while 
-
-*/
 k := FiniteField(2);
-planes := {@ EchelonForm(M) : M in Hom(VectorSpace(k, 3), VectorSpace(k, 6)) | Rank(M) eq 3 @};  
-
-fourlines := [];
 lines := ReadLinesIndex();
 
+// The 1395 echelon forms cutting out planes in P5.
+planes := {@ EchelonForm(M) : M in Hom(VectorSpace(k, 3), VectorSpace(k, 6)) | Rank(M) eq 3 @};  
+
+// A choice of four lines on each plane.
+fourlines := [];
 for i in  [1..#planes] do
-l := [ln : ln in lines | RowSpace(planes[i]) subset RowSpace(ln) ];
-fourlines[i] := {Index(lines,l[1]), Index(lines,l[2]), Index(lines,l[3]), Index(lines,l[4])};
+    l := [ln : ln in lines | RowSpace(planes[i]) subset RowSpace(ln) ];
+    fourlines[i] := {Index(lines, l[j]) : j in [1..4]};
 end for;
 
-//Eventually replace with a .m script reading in serialized data.
-load "readlinesinorbitsdata.m";
 
-linesthrough := linesinorbitsdata;
+function LoadLinesThroughCubics()
+    // If the lines-compute.m script has been run, load the results.
+    // i.e., load a list of lists `L` such that L[i][j] is the set of lines through the
+    // (i,j)-th orbit representative (using the non-flattened indexing of orbit representatives).
 
+    linesdir := DatabaseDirectory() * "linear_subspaces/lines_through_cubics/";
+    echforms := ReadLinesIndex();
+
+    result := [];
+    for i in [1..85] do
+        fname := Sprintf("lines-%o.data", i);
+        fcontents := Read(linesdir * fname);
+
+        // Parse the lists of indices.
+        linesThroughLists := eval fcontents;
+
+        // Obtain the corresponding echelon forms.
+        subresult := [[echforms[ind] : ind in indices] : indices in linesThroughLists];
+
+        // Append.
+        Append(~result, subresult);
+    end for;
+
+    return result;
+end function;
+
+linesthrough := LoadLinesThoughCubics();
+
+// The plane P goes through the i-th cubic if and only if four lines on that plane are contained
+// in the i-th cubic. (The indexing is with respect to whatever indexing was used for
+// linesthrough.)
 planesthrough := [];
 time for i in [1..#linesthrough] do
     planesthrough[i] := [];
-    for j in [1..1395] do
+    for j in [1..#planes] do
         if fourlines[j] subset linesthrough[i] then
-            planesthrough[i] :=  planesthrough[i] cat [planes[j]];
+            Append(~planesthrough[i], planes[j]);
         end if;
     end for;
-    if (i mod 10000) eq 0 then print i;
-    end if;
-end for;
-print "data computation finished";
 
+    // Report.
+    if (i mod 10000) eq 0 then print i; end if;
+end for;
+print "Incidence computation for planes finished.";
+
+
+// Compute indices for more efficient storage.
 planesthroughindexed := [];
 planeindices := AssociativeArray();
 for i in [1..#planes] do
-planeindices[planes[i]] := i;
+    planeindices[planes[i]] := i;
 end for;
 
 for i in [1..#planesthrough] do
-planesthroughindexed[i] := [planeindices[p]: p in planesthrough[i]];
+    planesthroughindexed[i] := [planeindices[p]: p in planesthrough[i]];
 end for;
 
-filename := "../..//database/linear_subspaces/planes_through_cubics/nonserialized-planes-data.csv";
+// Save the data.
 SetColumns(0);
-time for i in [1..#planesthroughindexed] do
-Write(filename, planesthroughindexed[i]);
+for i in [1..#planesthroughindexed] do
+    datadir := DatabaseDirectory();
+    subpath := "linear_subspaces/planes_through_cubics/";
+    fname := Sprintf("planes-%o.data", i);
+    Write(filename, planesthroughindexed[i]);
 end for;
-print "data file writing finished";
+
 
 
